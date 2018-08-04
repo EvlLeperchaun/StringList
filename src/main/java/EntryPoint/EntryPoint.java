c78f9bb34a35d8473e8debf2b442efc3f5c5dc43
EvlLeperchaun <evlleperchaun@gmail.com>
2018-08-04 13:41:34 -0400

    Added more add tests in main

--- src/main/java/EntryPoint/EntryPoint.java
+++ src/main/java/EntryPoint/EntryPoint.java
@@ -8,6 +8,9 @@ public class EntryPoint {
         CustomList list2 = new CustomList(2);
         list1.add("test");
         list1.add("test2");
+        list1.add("test3");
+        String x="test4";
+        list1.add(x);
         for (int i = 0; i < list1.size(); i++) {
             System.out.printf("Value:%s\n", list1.getValue(i));
 
